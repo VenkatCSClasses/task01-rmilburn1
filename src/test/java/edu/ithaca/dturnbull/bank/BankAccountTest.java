e3b8aa2650e6d59f562645e10755bc07345ef9a5
Ray Milburn <ray.milburn@icloud.com>
2026-01-28 10:47:07 -0500

    Wrote 21 tests for BankAccount.isEmailValid()

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -27,7 +27,38 @@ class BankAccountTest {
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
         assertFalse( BankAccount.isEmailValid(""));         // empty string
 
-        
+        // CHECKING @ SYMBOL
+        assertFalse( BankAccount.isEmailValid("ab.com"));    // missing @ symbol
+        assertFalse( BankAccount.isEmailValid("a@b@c.com")); // multiple @ symbols
+        assertFalse( BankAccount.isEmailValid("ab.com@"));   // @ symbol at end
+
+        // CHECKING PREFIX
+        assertFalse( BankAccount.isEmailValid("@b.com"));    // missing prefix
+        assertFalse( BankAccount.isEmailValid("a@bcom"));    // missing period
+        assertFalse( BankAccount.isEmailValid("a@b..com"));    // too many periods in domain
+        assertFalse( BankAccount.isEmailValid("a..b@c.com"));    // too many periods in prefix
+        assertFalse(BankAccount.isEmailValid(".a@b.com"));   // period at start of prefix
+
+        assertTrue( BankAccount.isEmailValid("A@b.com"));    // valid uppercase letter in prefix
+
+        // CHECKING DOMAIN
+        assertFalse( BankAccount.isEmailValid("a@.com"));    // missing domain
+        assertFalse( BankAccount.isEmailValid("a@b."));      // missing top-level domain
+        assertFalse( BankAccount.isEmailValid("a@b.c"));     // top-level domain too short
+
+        assertTrue( BankAccount.isEmailValid("a@b.co"));    // minimum valid email address
+        assertTrue( BankAccount.isEmailValid("a@b-c.com"));    // correct dash in domain
+        assertTrue( BankAccount.isEmailValid("a@b.org"));    // valid top-level domain
+
+        // CHECKING SPECIAL CHARACTERS
+        assertFalse( BankAccount.isEmailValid("a#@b.com"));  // invalid character in prefix
+        assertFalse( BankAccount.isEmailValid("a@b!.com"));  // invalid character in domain
+        assertFalse( BankAccount.isEmailValid("a-@b.com")); // invalid character at end of prefix
+
+        assertTrue( BankAccount.isEmailValid("a_b@c.com"));    // valid underscore in prefix
+        assertTrue( BankAccount.isEmailValid("a.b@c.com"));    // valid period in prefix
+        assertTrue( BankAccount.isEmailValid("a-b@c.com"));    // valid dash in prefix
+
     }
 
     @Test
